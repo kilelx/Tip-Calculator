a4fc45e90f5525ef086b149a474d694a1eca8580
kilelx <llx.kieran@gmail.com>
2022-01-27 15:14:48 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS3
+- JavaScript
+
+### What I learned
+
+The JS part was a bit too difficult for me : I had no idea where to begun. I had to watch a tutorial on Youtube first, and the redo it by myself. I also still have some difficulties with the RegEx.
+
+## Author
+
+- Website - [Kieran LELEUX](http://www.kieran-leleux.com)
+- Frontend Mentor - [@kilelx](https://www.frontendmentor.io/profile/kilelx)
---
+++ app.js
@@ -0,0 +1,140 @@
+const bill = document.querySelector("#bill-input");
+const errMessage = document.querySelectorAll(".message-error");
+const tips = document.querySelectorAll(".tip");
+const custTip = document.querySelector("#custom-tip");
+const nbPeople = document.querySelector("#people-input");
+const btnReset = document.querySelector("#reset");
+
+
+// All the event listeners
+bill.addEventListener("input", setBillValue);
+
+tips.forEach((btn) => {
+    btn.addEventListener("click", setTipPercent)
+});
+
+custTip.addEventListener("input", setCustomTip);
+
+nbPeople.addEventListener("input", setNbOfPeople);
+
+btnReset.addEventListener("click", reset);
+
+// Validate the input with RegEx
+
+function validBill(flt) {
+    let rgx = /^[0-9]*\.?[0-9]*$/;
+    return rgx.test(flt);
+}
+
+function validTip(tip) {
+    let rgx = /^[0-9]*$/;
+    return rgx.test(tip);
+}
+
+function validPeople(ppl) {
+    let rgx = /^[0-9]*$/;
+    return rgx.test(ppl);
+}
+
+// Declaring the variables
+
+let billValue = 0.00;
+let tipPercent = 0.15;
+let peopleValue = 2;
+
+// All the functions
+
+function setBillValue() {
+    if(bill.value.includes(',')) {
+        bill.value = bill.value.replace(',', '.');
+    }
+
+    if(!validBill(bill.value)) {
+        errMessage[0].classList.add("show-error-message");
+        setTimeout( function() {
+            errMessage[0].classList.remove("show-error-message");
+        }, 3000);
+        bill.value = 0.0;
+    } else {
+        billValue = bill.value;
+    }
+
+    setTipAmount();
+
+}
+
+function setTipPercent(event) {
+
+    tips.forEach((btn) => {
+
+        // Removing the active state
+        btn.classList.remove("active");
+
+        // In order to select only the clicked button, we verify the value
+        if(event.target.innerHTML == btn.innerHTML) {
+            btn.classList.add("active");
+            tipPercent = parseFloat(btn.innerHTML)/100;
+            console.log(tipPercent);
+        }
+    })
+
+    setTipAmount();
+
+}
+
+function setCustomTip() {
+
+    // Validate the custom tip
+    if(!validTip(custTip.value)) {
+        console.log("erreur regex");
+    }
+
+    tipPercent = parseFloat(custTip.value)/100;
+    console.log(tipPercent);
+
+    tips.forEach(btn => {
+        btn.classList.remove("active");
+    })
+
+    if (tipCustom.value !== '') {
+        setTipPercent();
+    }
+
+}
+
+function setNbOfPeople() {
+
+    if(!validPeople(nbPeople.value)) {
+        errMessage[1].classList.add("show-error-message");
+        setTimeout( function() {
+            errMessage[1].classList.remove("show-error-message");
+        }, 3000);
+        nbPeople.value = 0.0;
+    } else {
+        peopleValue = nbPeople.value;
+    }
+
+    setTipAmount();
+
+}
+
+function setTipAmount() {
+
+    btnReset.removeAttribute("disabled");
+
+    if (peopleValue > 0) {
+        let tipAmount = (billValue * tipPercent) / peopleValue;
+        let totalAmount = (billValue / peopleValue) + tipAmount;
+        document.querySelector("#value-tip").innerHTML = (tipAmount.toFixed(2)+ "$");
+        document.querySelector("#value-total").innerHTML = (totalAmount.toFixed(2)+ "$");
+    }
+}
+
+function reset() {
+
+    bill.value = 0;
+    tips[2].click();
+    nbPeople.value = 2;
+
+    btnReset.setAttribute("disabled", "true");
+}
